664f6499be260c25f5201caad48f29239cbf4025
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-11 17:32:11 +0200

    bug: Fix bugs introduced by typos.

--- models/Score.js
+++ models/Score.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const scoreSchema = new mongoose.Schema({
 	username: { type: String, required: true },
-	totalTime: { type: number, required: true },
-	lastGame: { type: boolean, required: true },
+	totalTime: { type: Number, required: true },
+	lastGame: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('score', scoreSchema);
