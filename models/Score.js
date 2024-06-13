797d8301044e97d9e9ba2f6f0e66bdc061e819c5
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-13 23:14:10 +0200

    feat: username field is optional.

--- models/Score.js
+++ models/Score.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const scoreSchema = new mongoose.Schema({
-	username: { type: String, required: true },
+	username: { type: String, required: false },
 	totalTime: { type: Number, required: true },
 	lastGame: { type: Boolean, required: true },
 });
